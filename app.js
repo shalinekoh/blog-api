558fc6f8098db1b25ef1f24de58ec171b99bacc7
shalinekoh <kohshaline@gmail.com>
2024-11-14 17:53:45 +0800

    [Fix] Remove cors option

--- app.js
+++ app.js
@@ -31,7 +31,7 @@ if (process.env.NODE_ENV === "development") {
 app.use(express.json());
 app.use(cookieParser());
 
-app.options("*", cors(corsOptions));
+app.options("*", cors());
 
 app.use(router);
 
